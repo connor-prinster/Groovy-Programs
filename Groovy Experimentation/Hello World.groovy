1c384569bde116cc545185478d0de2df0341bae0
Connor Prinster <ConnorPrinster@ldschurch.org>
2018-05-14 16:38:22 -0600

    Removed semicolons and adjusted types

--- Groovy Experimentation/Hello World.groovy
+++ Groovy Experimentation/Hello World.groovy
@@ -2,43 +2,42 @@ class Example
 {
     static def simpleMethod()
     {
-        System.out.println("I made it inside the method!");
+        println("I made it inside the method!" as Object)
 
-        System.out.println("Hello, World");
+        System.out.println("Hello, World" as Object)
 
-        def a = 5;
-        def A = 3;
-        String b = 'The result of ' + a + '%' + A + ' equals ' + a%3;
-        System.out.println(b);
+        int a = 5
+        int A = 3
+        String b = 'The result of ' + a + '%' + A + ' equals ' + a%3
+        System.out.println(b as Object)
 
-        def range = 5..10;
-        System.out.println(range);
+        def range = 5..10
+        System.out.println(range as Object)
 
         for(int i = 0; i < range.size(); i++)
         {
-            System.out.println(range.get(i));
+            System.out.println(range.get(i))
         }
 
-        System.out.println("\nPrinting from simpleMethodWithParameters");
+        System.out.println("\nPrinting from simpleMethodWithParameters" as Object)
         for(int i = 0; i < 2; i++)
         {
             for(int j = 0; j < 2; j++)
             {
-                simpleMethodWithParameters(i, j);
+                simpleMethodWithParameters(i, j)
             }
         }
 
-        System.out.println("\nPrinting from simpleIntMethod");
+        System.out.println("\nPrinting from simpleIntMethod" as Object)
         for(int i = 0; i < 2; i++)
         {
             for(int j = 0; j < 2; j++)
             {
-                System.out.println("The summation of " + j + " + " + i + " = " + simpleIntMethod(i, j));
+                System.out.println("The summation of " + j + " + " + i + " = " + simpleIntMethod(i, j) as Object)
             }
         }
 
-        System.out.println("\nPrinting from simpleGstringMethod
-    ");
+        System.out.println("\nPrinting from simpleGstringMethod " as Object)
         for(int i = 0; i < 2; i++)
         {
             for(int j = 0; j < 2; j++)
@@ -52,24 +51,24 @@ class Example
     //can return typeless 'def'
     static def simpleMethodWithParameters(int a, int b)
     {
-        int summation = a+b;
-        String dataToPrint = "The summation of " + a + " + " + b + " = " + summation;
-        System.out.println(dataToPrint);
+        int summation = a+b
+        String dataToPrint = "The summation of " + a + " + " + b + " = " + summation
+        System.out.println(dataToPrint as Object)
     }
 
     //can return int
     static int simpleIntMethod(int a, int b)
     {
-        return a + b;
+        return a + b
     }
 
     static void simpleGstringMethod(int a, int b)
     {
-        System.out.println("The summation of $a and $b is ${a+b}")
+        System.out.println("The summation of $a and $b is ${a+b}" as Object)
     }
 
     static void main(String[] args)
     {
-        simpleMethod();
+        simpleMethod()
     }
 }
