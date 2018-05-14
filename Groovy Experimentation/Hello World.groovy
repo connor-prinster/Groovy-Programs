d766263a314137b0ffeae735eab4df9831cbf1a6
Connor Prinster <ConnorPrinster@ldschurch.org>
2018-05-14 13:15:52 -0600

    turns out there's such thing as a range as well as a for loop

--- Groovy Experimentation/Hello World.groovy
+++ Groovy Experimentation/Hello World.groovy
@@ -4,3 +4,9 @@ def a = 5;
 def A = 3;
 String b = 'The result of ' + a + '%' + A + ' equals ' + a%3;
 System.out.println(b)
+
+def range = 5..10
+System.out.println(range)
+
+for(int i = 0; i < range.size(); i++)
+    System.out.println(range.get(i))
